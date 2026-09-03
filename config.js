cb45b815d2dbd20ec622dfde5ad55c2756f77cee
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-09-03 10:56:33 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 window.DPRO_CONFIG = Object.freeze({
   systemId:'dpro_pest_env',
   systemName:'DPRO ペストコントロール／環境衛生サービス',
-  version:'1.1.0-g6prod',
+  version:'1.3.0-g7-ready',
   apiBase:'https://dpro-pest-env-core.dpromstk2000.workers.dev',
   demoQuery:'demo=1',
   productionConfigured:true,
   lineEnabled:false,
+  lineLiffId:'',
   supportPhone:'',
   supportEmail:''
 });
